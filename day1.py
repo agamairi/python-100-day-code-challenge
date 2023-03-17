6a7311027e73109d3a5d0c0c2e144749384c148c
Agam Airi <75530266+agamairi@users.noreply.github.com>
2023-03-16 23:37:21 -0400

    day1

---
+++ day1.py
@@ -0,0 +1,16 @@
+# string manipulation and playing with the print function in python
+
+print("hello world\nhello hello world\nhello hello hello world")
+print("print('what to print')")
+print("hello" + "world")
+
+# exercise : band name generator
+
+cityName = input("what is the name of the city you grew up in?\n")
+petName = input("what is the name of your pet?\n")
+
+print("your band name is: " + cityName + " " + petName)
+
+
+# day 1 complete.
+# we talked about variables and string manipulation and also about taking user input in and printing it using the print function.
